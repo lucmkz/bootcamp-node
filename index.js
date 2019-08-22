fab184902fda6f62df9e4e4df3e4c665809afb29
Lucas <l.duarte.mk@gmail.com>
2019-08-22 17:11:45 -0300

    crud complet

--- index.js
+++ index.js
@@ -2,10 +2,42 @@ const express = require("express");
 
 const server = express();
 
-server.get("/teste", (req, res) => {
-  return res.json({
-    message: "hello world"
-  });
+server.use(express.json());
+
+const users = ["Lucas", "Duarte", "Santos", "Dos"];
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+  return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.json(users);
 });
 
 server.listen(3000);
